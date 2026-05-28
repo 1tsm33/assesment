6032b0786cbf9cd9182a0e7cc897a1e8b2b33df5
1tsm33 <jordaank4348@masseyhigh.school.nz>
2026-05-28 20:06:18 +1200

    1 string_cheker.py

--- string_cheker.py
+++ string_cheker.py
@@ -1,7 +1,7 @@
 # Check that user have entered a valid
 # option base on a list
 def instructions():
-    print("""if you choose (*) that is multiplication.
+    print("""if you choose (x) that is multiplication.
              If you choose (+) that is addition.
              And if you choose (-) that is subtraction.""")
 
