53fc2f328d3b2cf3a478635bf6621269665f1911
1tsm33 <jordaank4348@masseyhigh.school.nz>
2026-05-29 19:23:08 +1200

    equation gen.py

--- equation gen.py
+++ equation gen.py
@@ -1,22 +1,21 @@
-# question generator
+#Question generator
 def generate_question():
-  number1 = random.randint(low,high)
-  number2 = random.randint(low,high)
-  operation = random.choice(ops)
+   number1 = random.randint(low,high)
+   number2 = random.randint(low,high)
+   operation = random.choice(ops)
 
-#Operations
-if operation=="x":
-  ans = number1*number2
-elif operation=="+":
-  ans = number1+number2
-elif operation=="-":
-  ans = number1-number2
-else:
-  ans = number1
-  number1 = number1*number2
-
-# Question
-question = f"What is {number1} {operation} {number2}? "
-
-return question, answer 
+   #Operations
+   if operation=="x":
+     ans = number1*number2
+   elif operation=="+":
+     ans = number1+number2
+   elif operation=="-":
+     ans = number1-number2
+   else:
+     ans = number1
+     number1 = number1*number2
+  
+  #Question
+   question = f"What is {number1} {operation} {number2}?"
+   return question, answer 
 
